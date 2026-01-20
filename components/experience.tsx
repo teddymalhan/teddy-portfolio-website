621fe9ec5689513f927bf615885f64006ae5403d
Teddy Malhan <ama367@sfu.ca>
2026-01-20 14:53:12 -0800

    fix(experience): update tag for EADP Arrival to reflect new branding as Digital Platform: Arrival

--- components/experience.tsx
+++ components/experience.tsx
@@ -23,7 +23,7 @@ const experiences: ExperienceItem[] = [
     location: "Vancouver, BC",
     logo: "/ea-logo-full.jpg",
     logoStyle: "padded",
-    tags: ["🕹️ EADP Arrival"],
+    tags: ["☁️ Digital Platform: Arrival"],
   },
   {
     company: "Dialpad",
